3d16c0a94ebcd76e44691e166232d5d5beaf15db
UJeans <jean207@naver.com>
2024-07-26 16:36:10 +0900

    [#9]feat: DB-server에게 저장 요청

--- src/main/java/soma/haeya/edupi_user/signup/SignUpController.java
+++ src/main/java/soma/haeya/edupi_user/signup/SignUpController.java
@@ -2,10 +2,13 @@ package soma.haeya.edupi_user.signup;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.ModelAndView;
 import soma.haeya.edupi_user.signup.dto.SignUpDTO;
 
@@ -15,10 +18,21 @@ import soma.haeya.edupi_user.signup.dto.SignUpDTO;
 public class SignUpController {
 
     @RequestMapping(value = "signup", method = RequestMethod.POST)
-    public String createPost(@Valid @RequestBody SignUpDTO signUpDTO) {
+    public ResponseEntity<Void> createPost(@Valid @RequestBody SignUpDTO signUpDTO) {
         ModelAndView mav = new ModelAndView();
 
         log.info("[SignUpDTO] {}", signUpDTO);
-        return "ok";
+
+        // DB에 저장하기
+        RestClient restClient = RestClient.create();
+
+        ResponseEntity<Void> bodilessEntity = restClient.post()
+                .uri("http://localhost:8081/save/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(signUpDTO)
+                .retrieve()
+                .toBodilessEntity();
+
+        return bodilessEntity;
     }
 }
