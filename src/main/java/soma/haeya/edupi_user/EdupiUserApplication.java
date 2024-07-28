d9a46d111b2057ac600fcf1740199b66ad5a5009
UJeans <jean207@naver.com>
2024-07-28 13:23:57 +0900

    [#9]fix: EduPiUserApplication 이름 변경

--- src/main/java/soma/haeya/edupi_user/EdupiUserApplication.java
+++ src/main/java/soma/haeya/edupi_user/EdupiUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EdupiUserApplication {
+public class EduPiUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EdupiUserApplication.class, args);
+		SpringApplication.run(EduPiUserApplication.class, args);
 	}
 
 }
