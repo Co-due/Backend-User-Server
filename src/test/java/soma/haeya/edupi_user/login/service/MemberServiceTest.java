c3138107cef130bbba7df23470156a3391c0b015
GS_song98 <20003204@sju.ac.kr>
2024-07-29 15:07:58 +0900

    [#7]feat: MemberService 기능 구현 및 테스트

---
+++ src/test/java/soma/haeya/edupi_user/login/service/MemberServiceTest.java
@@ -0,0 +1,72 @@
+package soma.haeya.edupi_user.login.service;
+
+import static org.mockito.Mockito.when;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import soma.haeya.edupi_user.login.auth.TokenProvider;
+import soma.haeya.edupi_user.login.client.MemberApiClient;
+import soma.haeya.edupi_user.login.domain.Member;
+import soma.haeya.edupi_user.login.domain.Role;
+import soma.haeya.edupi_user.login.dto.MemberLoginRequest;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceTest {
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberApiClient memberRepository;
+
+    @Mock
+    private TokenProvider tokenProvider;
+
+    private MemberLoginRequest memberLoginRequest;
+
+    @BeforeEach
+    void init() {
+        memberLoginRequest = MemberLoginRequest.builder()
+            .email("asdf@naver.com")
+            .password("asdf1234")
+            .build();
+    }
+
+
+    @Test
+    @DisplayName("아이디와 패스워드에 맞는 멤버가 있으면 token을 반환한다.")
+    void memberLogin() {
+
+        Member expectedMember = new Member("asdf@naver.com", "홍길동", Role.ROLE_USER);
+
+        String expectedToken = "token";
+
+        when(memberRepository.findMemberByEmailAndPassword(memberLoginRequest)).thenReturn(
+            expectedMember);
+        when(tokenProvider.generateToken(expectedMember)).thenReturn(expectedToken);
+
+        String resultToken = memberService.memberLogin(memberLoginRequest);
+
+        Assertions.assertThat(resultToken).isEqualTo(expectedToken);
+    }
+
+    @Test
+    @DisplayName("아이디 패스워드에 맞는 멤버가 없으면 예외를 반환한다.")
+    void memberLoginException() {
+
+        when(memberRepository.findMemberByEmailAndPassword(memberLoginRequest)).thenThrow(
+            new IllegalArgumentException("아이디 비밀번호가 일치하지 않습니다.")
+        );
+
+        Assertions.assertThatThrownBy(() -> memberService.memberLogin(memberLoginRequest))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("아이디 비밀번호가 일치하지 않습니다.");
+    }
+
+}
