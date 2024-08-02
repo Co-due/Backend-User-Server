b7c6e9c2cd806a53dc7eb7ca72c42627e1d5c740
UJeans <jean207@naver.com>
2024-08-02 15:47:22 +0900

    [#9]delete: ErrorResponse, SuccessResponse

--- src/main/java/soma/haeya/edupi_user/dto/response/ErrorResponse.java
+++
@@ -1,4 +0,0 @@
-package soma.haeya.edupi_user.dto.response;
-
-public record ErrorResponse(String message) {
-}
--- src/main/java/soma/haeya/edupi_user/dto/response/SuccessResponse.java
+++
@@ -1,4 +0,0 @@
-package soma.haeya.edupi_user.dto.response;
-
-public record SuccessResponse(String message) {
-}
