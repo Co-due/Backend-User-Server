072905c51003daee3d63ef82975bbfcc18ba94b6
UJeans <jean207@naver.com>
2024-08-04 16:00:24 +0900

    [#13]test: controller 회원가입 실패

--- src/test/java/soma/haeya/edupi_user/controller/MemberControllerTest.java
+++ src/test/java/soma/haeya/edupi_user/controller/MemberControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import soma.haeya.edupi_user.dto.request.MemberLoginRequest;
 import soma.haeya.edupi_user.dto.request.SignupRequest;
 import soma.haeya.edupi_user.dto.response.Response;
+import soma.haeya.edupi_user.exception.DbValidException;
 import soma.haeya.edupi_user.service.MemberService;
 
 @WebMvcTest(MemberController.class)
@@ -61,8 +62,9 @@ class MemberControllerTest {
   }
 
   @Test
-  @DisplayName("회원가입에 성공하면 성공 요청을 보낸다")
+  @DisplayName("회원가입에 성공하면 OK를 반환한다.")
   void signUp() throws Exception {
+    // given
     SignupRequest signupRequest = SignupRequest.builder()
         .email("aabbcc@naver.com")
         .name("김미미")
@@ -76,12 +78,35 @@ class MemberControllerTest {
             .body(new Response("회원가입 성공"))
     );
 
+    // When & Then
     mockMvc.perform(post("/member/signup")
         .content(objectMapper.writeValueAsString(signupRequest))
         .contentType(MediaType.APPLICATION_JSON)
         .accept(MediaType.APPLICATION_JSON)
     ).andExpect(status().isOk());
 
+  }
+
+  @Test
+  @DisplayName("잘못된 요청으로 예외가 발생하면 BAD_REQUEST를 반환한다")
+  void signUp_whenInvalidRequest_thenBadRequest() throws Exception {
+    // Given
+    SignupRequest signupRequest = SignupRequest.builder()
+        .email("invalid-email")  // 유효하지 않은 이메일
+        .name("김미미")
+        .password("qpwoeiruty00@")
+        .build();
+
+    // Mocking
+    when(memberService.signUp(signupRequest))
+        .thenThrow(new DbValidException("Invalid email address"));
+
+    // When & Then
+    mockMvc.perform(post("/member/signup")
+            .content(objectMapper.writeValueAsString(signupRequest))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest()); // 예외 처리 후 상태 코드가 400인지 검증
   }
 
 
